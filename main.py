6ed2e16c261eed1aaf5d5b88b9157fa6f07f23aa
GitHub Action <action@github.com>
2020-08-16 15:12:43 +0800

    v1

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import json
+import os
+from pathlib import Path
+
+from mpkg.load import Load
+
+for repo in ['main', 'extras']:
+    os.system(
+        f'wget -q https://github.com/mpkg-project/mpkg-autobuild/releases/download/AutoBuild/{repo}.json')
+    softs = Load(f'{repo}.json')[0]
+    root = Path(repo)
+    if not root.exists():
+        root.mkdir()
+    for soft in softs:
+        with open(root/(soft['id']+'.json'), 'w', encoding='utf8') as f:
+            f.write(json.dumps(soft, indent=4))
+
+os.system(f'wget -q https://github.com/mpkg-project/mpkg-autobuild/releases/download/AutoBuild/warning.txt')
